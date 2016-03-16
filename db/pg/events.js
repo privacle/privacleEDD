6cfc44b8fb0ed5762ea440413ebec25fcaa9d581
dlawr <dan.lawrence3@gmail.com>
2016-03-16 14:54:39 -0400

    get one event route and function work

--- db/pg/events.js
+++ db/pg/events.js
@@ -36,5 +36,32 @@ function newEvents(req, res, next) {
     })
 }
 
+function myEvents(req, res, next) {
+  db.any(`select * from events where owner like $1`,
+    [req.params.user_id])
+  .then(function(data) {
+    res.events = data;
+    next();
+  })
+  .catch(function(err){
+    console.error('error with select * from events where owner like $/owner/', err);
+  })
+}
+
+function oneEvent(req, res, next) {
+  req.params.event_id = parseInt(req.params.event_id);
+  db.one(`select * from events where event_id = $/event_id/`,
+    req.params)
+    .then(function(data) {
+      res.event = data;
+      next();
+    })
+    .catch(function(err) {
+      console.error('error with insert into events',err);
+    })
+}
+
 module.exports.allEvents = allEvents;
 module.exports.newEvents = newEvents;
+module.exports.myEvents = myEvents;
+module.exports.oneEvent = oneEvent;
--- routes/events.js
+++ routes/events.js
@@ -11,13 +11,13 @@ events.route('/')
   // All events created by current user and user's friends
   .post( db.newEvents, (req,res)=>res.json(res.event_id) )
 
-// events.route('/myevents/:user_id')
-//   .get( db.myEvents, (req,res)=>res.json(res.rows) )
-//   // Show all events created/added by current user
+events.route('/myevents/:user_id')
+  .get( db.myEvents, (req,res)=>res.json(res.events) )
+  // Show all events created/added by current user
 
-// events.route('/:event_id')
-//   // RUD a specific event created by current user
-//   .get( db.oneEvent, (req,res)=>res.json(res.rows) )
+events.route('/:event_id')
+  // RUD a specific event created by current user
+  .get( db.oneEvent, (req,res)=>res.json(res.event) )
 //   .put( db.editEvent, (req,res)=>res.json(res.rows) )
 //   .delete( db.deleteEvent, (req,res)=>res.json(res.rows) )
 //
