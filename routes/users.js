9cb71ed710a53f0e87379d213bc6169e36fa9c3f
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-03-16 15:21:09 -0400

    added express jwt and json web tokens

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,10 @@ const users      = express.Router();
 const bodyParser = require('body-parser');
 const db         = require('./../db/pg/users');
 
+const secret      = "sweet sweet secret";
+const expressJWT  = require('express-jwt');
+const jwt         = require('jsonwebtoken');
+
 users.route('/')
 //   .get( db.allUsers, (req,res)=>res.json(res.rows) )
 //   // Only admin can get all users from the database
@@ -12,7 +16,10 @@ users.route('/')
   // Create a new user
 
 users.route('/login')
-  .post( db.login, (req,res)=>res.json(res.rows) )
+  .post( db.login, (req,res)=> {
+    var token = jwt.sign(res.rows, secret);
+    res.json({users: res.rows, token: token});
+  })
 
 // users.route('/logout')
 //   .delete( db.logout, (req,res)=>res.json(res.rows) )
