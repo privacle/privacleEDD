8603a416faf3d0a9fcf9f8110a62c7eedd570325
Douglas Wissett Walker <douglas.wissett@gmail.com>
2016-03-18 10:30:24 -0400

    added search states + ajax requests to grab events/users

--- public/js/components/find.js
+++ public/js/components/find.js
@@ -3,12 +3,93 @@ const auth = require('../auth');
 
 const Find = React.createClass({
 
+  getInitialState : function() {
+    return {
+      events: {},
+      users: {}
+    }
+  },
+
   handleSubmit : function(event) {
     event.preventDefault();
     
+    let eventSearch   = this.refs.event.value;
+    let eventSearchId = this.refs.event_id.value;
+    let userSearch    = this.refs.user_email.value;
+    let userSearchId  = this.refs.user_id.value;
 
-    var event = this.refs.event.value;
-    var user = this.refs.user.value;
+    if(eventSearch || eventSearchId) {
+      if(eventSearch) {
+        $.ajax({
+          url: '/api/events/name/' + eventSearch,
+          type: 'GET',
+          beforeSend: function( xhr ) {
+            xhr.setRequestHeader("Authorization", 'Bearer ' + auth.getToken() );
+          }
+        })
+        .done((data) => {
+          console.log('event search name: ', data);
+
+          data.forEach((el) => {
+            this.state.events[el.event_id] = el;
+            this.setState({ events: this.state.events });
+          });
+        })
+      } else {
+        $.ajax({
+          url: '/api/events/id/' + eventSearchId,
+          type: 'GET',
+          beforeSend: function( xhr ) {
+            xhr.setRequestHeader("Authorization", 'Bearer ' + auth.getToken() );
+          }
+        })
+        .done((data) => {
+          console.log('event search id: ', data);
+
+          data.forEach((el) => {
+            this.state.events[el.event_id] = el;
+            this.setState({ events: this.state.events });
+          });
+        })
+      }
+    } else {
+      if(userSearch) {
+        $.ajax({
+          url: '/api/users/email/' + userSearch,
+          type: 'GET',
+          beforeSend: function( xhr ) {
+            xhr.setRequestHeader("Authorization", 'Bearer ' + auth.getToken() );
+          }
+        })
+        .done((data) => {
+          console.log('user search name: ', data);
+
+          data.forEach((el) => {
+            this.state.users[el.user_id] = el;
+            this.setState({ users: this.state.users });
+          });
+        })
+      } else {
+        $.ajax({
+          url: '/api/users/id/' + userSearchId,
+          type: 'GET',
+          beforeSend: function( xhr ) {
+            xhr.setRequestHeader("Authorization", 'Bearer ' + auth.getToken() );
+          }
+        })
+        .done((data) => {
+          console.log('user search id: ', data);
+
+          data.forEach((el) => {
+            this.state.users[el.user_id] = el;
+            this.setState({ users: this.state.users });
+          });
+        })
+      }
+    }
+
+    this.refs.eventSearchForm.reset();
+    this.refs.userSearchForm.reset();
   },
 
   hideSearchform : function(){
@@ -21,14 +102,16 @@ const Find = React.createClass({
         <h1>Find stuff</h1>
         <div id="searchform" className="card-panel" style={{width: '80%', margin: 'auto'}}>
           <div>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" ref="event" placeholder="Search by event" />
+            <form onSubmit={this.handleSubmit} ref="eventSearchForm">
+              <input type="text" ref="event" placeholder="Search by event name" />
+              <input type="text" ref="event_id" placeholder="Search by event ID" />
               <button type="submit" onClick={this.hideSearchform} className="btn waves-effect waves-light light-blue darken-4">Search</button>
             </form>
           </div>
           <div>
-            <form onSubmit={this.handleSubmit}>
-              <input type="text" ref="user" placeholder="Search by user" />
+            <form onSubmit={this.handleSubmit} ref="userSearchForm">
+              <input type="text" ref="user_email" placeholder="Search by user email" />
+              <input type="text" ref="user_id" placeholder="Search by user ID" />
               <button type="submit" onClick={this.hideSearchform} className="btn waves-effect waves-light light-blue darken-4">Search</button>
             </form>
           </div>
