9250fec64696249c52417d1b1c2db44d0d6d9a0f
Emma Hou <emma.houy@gmail.com>
2016-03-18 10:58:19 -0400

    add friend cards

--- public/js/components/friends.js
+++ public/js/components/friends.js
@@ -7,27 +7,33 @@ const Friends = React.createClass({
     return (
       <div>
         <h1>My Friends</h1>
-        <div className="row">
-          <div className="col s12 m3" data-reactid=".0.1.2.0" style={{marginLeft: 100, width: '20%'}}>
-            <div className="card">
-              <div className="card-image">
-                <img src="http://barkpost.com/wp-content/uploads/2015/02/wilburs_world.jpg" />
-                <span className="card-title">Event1</span>
-              </div>
-              <div className="card-content">
-                <ul>
-                  <li className="text-muted">Date: 11-11-1111</li>
-                  <li className="text-muted">Time: 11:11</li>
-                  <li className="text-muted">Location: New York</li>
-                  <li className="text-muted">Description: Learn JS</li>
-                  <li className="text-muted">Created By: EDD </li>
-                </ul>
-                <div className="card-action">
-                <a href="#">This is a link</a>
+        <div className="col s12 m6 l4 row">
+          <ul>
+            <li>
+              <div id="profile-card" className="card" style={{marginTop: 30, marginLeft: 30, width: 250, height: 'auto', overflow: 'hidden'}}>
+                <div className="card-content">
+                  <img src="http://barkpost.com/wp-content/uploads/2015/02/wilburs_world.jpg" alt className="circle responsive-img activator card-profile-image" />
+                  <a className="btn-floating activator btn-move-up waves-effect waves-light darken-2 right">
+                  <i className="mdi-action-account-circle" />
+                  </a>
+                  <span className="card-title activator grey-text text-darken-4">Roger Waters</span>
+                  <p><i className="mdi-action-perm-identity cyan-text text-darken-2" /> Project Manager</p>
+                  <p><i className="mdi-action-perm-phone-msg cyan-text text-darken-2" /> +1 (612) 222 8989</p>
+                  <p><i className="mdi-communication-email cyan-text text-darken-2" /> mail@domain.com</p>
+                </div>
+
+                <div className="card-reveal" style={{display: 'none', transform: 'translateY(0px)'}}>
+                  <span className="card-title grey-text text-darken-4">Roger Waters <i className="mdi-navigation-close right" /></span>
+                  <p>Here is some more information about this card.</p>
+                  <p><i className="mdi-action-perm-identity cyan-text text-darken-2" /> Project Manager</p>
+                  <p><i className="mdi-action-perm-phone-msg cyan-text text-darken-2" /> +1 (612) 222 8989</p>
+                  <p><i className="mdi-communication-email cyan-text text-darken-2" /> mail@domain.com</p>
+                  <p><i className="mdi-social-cake cyan-text text-darken-2" /> 18th June 1990</p>
+                  <p><i className="mdi-device-airplanemode-on cyan-text text-darken-2" /> BAR - AUS</p>
                 </div>
               </div>
-            </div>
-          </div>
+            </li>
+          </ul>
         </div>
       </div>
     )}
