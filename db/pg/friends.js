7064e42f08ad242fec9f93232b7f7349a0ea316a
Emma Hou <emma.houy@gmail.com>
2016-03-20 14:24:51 -0400

    fix data

--- db/pg/friends.js
+++ db/pg/friends.js
@@ -1,17 +1,14 @@
 'use strict';
 const pgp = require('pg-promise')({});
 
-if(process.env.ENVIRONMENT === 'production'){
-  var connectionString = process.env.DATABASE_URL;
-}else{
-  const cn = {
-      host: process.env.HOST, // server name or IP address;
-      port: 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS
-  }
+const cn = {
+    host: process.env.HOST, // server name or IP address;
+    port: 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
 }
+
 
 const db = pgp(cn);
 
