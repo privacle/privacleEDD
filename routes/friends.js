7763e7bf58c54e8a3e9e51ce93472f7f89cb02e0
dlawr <dan.lawrence3@gmail.com>
2016-03-20 19:39:10 -0400

    route to hit addFriendToCircle

--- routes/friends.js
+++ routes/friends.js
@@ -8,9 +8,9 @@ const db         = require('./../db/pg/friends');
 
 friends.route('/')
   .post( db.newFriend, (req,res)=>res.json(res.rows) )
-  .delete( db.deleteFriend, (req,res)=>res.json(req.method))
+  .delete( db.deleteFriend, (req,res)=>res.json(req.method) )
 
 friends.route('/circles')
-  .get( db.myCircles, (req, res)=>res.json(res.circles))
-
+  .get( db.myCircles, (req, res)=>res.json(res.circles) )
+  .post( db.addFriendToCircle, (req,res)=>res.json(req.method) )
 module.exports = friends;
