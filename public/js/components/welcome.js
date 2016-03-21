440c05eaf31bfb7ad8c1d85d2e68dd4ad81330b0
Emma Hou <emma.houy@gmail.com>
2016-03-21 14:34:27 -0400

    change welcome page img

--- public/js/components/welcome.js
+++ public/js/components/welcome.js
@@ -5,7 +5,7 @@ const Welcome = React.createClass({
   render: function(){
     return (
       <div id="welcome">
-        <img src="../../img/background.png" alt="background-img" />
+        <img src="../../img/background4.jpg" alt="background-img" style={{width:"100%", marginBottom: "-30px"}} />
       </div>
     )
   }
